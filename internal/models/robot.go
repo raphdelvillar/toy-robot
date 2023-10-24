8266579187b50ce81c127bbb32e53ae95ba12536
Raph Wilson Del Villar <raph_delvillar@engagerocket.co>
2023-10-25 02:35:34 +0800

    add robot model

---
+++ internal/models/robot.go
@@ -0,0 +1,76 @@
+package robot
+
+import (
+	"log"
+
+	"github.com/raphdelvillar/toy-robot/enum"
+)
+
+type Robot struct {
+	XAxisLocation    int
+	YAxisLocation    int
+	CurrentDirection enum.Direction
+}
+
+func (r *Robot) InitialPlacement(xAxis int, yAxis int, direction enum.Direction) {
+	r.XAxisLocation = xAxis
+	r.YAxisLocation = yAxis
+	r.CurrentDirection = direction
+}
+
+func (r *Robot) MoveForward() {
+	switch r.CurrentDirection {
+	case enum.DirectionNorth:
+		r.YAxisLocation++
+		break
+	case enum.DirectionSouth:
+		r.YAxisLocation--
+		break
+	case enum.DirectionEast:
+		r.XAxisLocation++
+		break
+	case enum.DirectionWest:
+		r.XAxisLocation--
+		break
+	default:
+		log.Panic("not a valid direction")
+	}
+}
+
+func (r *Robot) TurnLeft() {
+	switch r.CurrentDirection {
+	case enum.DirectionNorth:
+		r.CurrentDirection = enum.DirectionEast
+		break
+	case enum.DirectionSouth:
+		r.CurrentDirection = enum.DirectionWest
+		break
+	case enum.DirectionEast:
+		r.CurrentDirection = enum.DirectionSouth
+		break
+	case enum.DirectionWest:
+		r.CurrentDirection = enum.DirectionNorth
+		break
+	default:
+		log.Panic("not a valid direction")
+	}
+}
+
+func (r *Robot) TurnRight() {
+	switch r.CurrentDirection {
+	case enum.DirectionNorth:
+		r.CurrentDirection = enum.DirectionWest
+		break
+	case enum.DirectionSouth:
+		r.CurrentDirection = enum.DirectionEast
+		break
+	case enum.DirectionEast:
+		r.CurrentDirection = enum.DirectionNorth
+		break
+	case enum.DirectionWest:
+		r.CurrentDirection = enum.DirectionSouth
+		break
+	default:
+		log.Panic("not a valid direction")
+	}
+}

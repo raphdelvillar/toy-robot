cc291bd384c9b2df9c6e70379b802e691d7ff407
Raph Wilson Del Villar <raph_delvillar@engagerocket.co>
2023-10-25 02:35:41 +0800

    add direction enum

---
+++ enum/direction.go
@@ -0,0 +1,10 @@
+package enum
+
+type Direction string
+
+const (
+	DirectionNorth Direction = "North"
+	DirectionSouth Direction = "South"
+	DirectionWest  Direction = "West"
+	DirectionEast  Direction = "East"
+)

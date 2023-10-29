ce30cd4344bfbc7b5b724b0bc7c789d160737ef9
Raph Wilson Del Villar <raph_delvillar@engagerocket.co>
2023-10-29 23:16:50 +0800

    add user input and command list

--- client/app/components/Log.tsx
+++ client/app/components/Log.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Textarea } from "@nextui-org/react";
 
 const Log = () => {
     return (
-        <div></div>
+        <div>
+            <Textarea
+                isDisabled
+                labelPlacement="outside"
+                className="max-w-xs"
+            />
+        </div>
     )
 }
 
--- client/app/components/UserInput.tsx
+++ client/app/components/UserInput.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { Input } from "@nextui-org/react";
+import {Chip} from "@nextui-org/react";
+import {Spacer} from "@nextui-org/react";
 
 const UserInput = () => {
     return (
         <div>
+            <div className="flex">
+                <Chip>PLACE X,Y,F</Chip>
+                <Spacer x={2} />
+                <Chip>MOVE</Chip>
+                <Spacer x={2} />
+                <Chip>LEFT</Chip>
+                <Spacer x={2} />
+                <Chip>RIGHT</Chip>
+            </div>
+            <Spacer x={4} />
             <Input placeholder="Enter your command here" />
         </div>
     )

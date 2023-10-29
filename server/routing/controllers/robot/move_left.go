b295b4b5579cdfbce5f13a4024d3e07d93eba6d7
Raph Wilson Del Villar <raph_delvillar@engagerocket.co>
2023-10-29 22:26:48 +0800

    add move left logic

--- server/routing/controllers/robot/move_left.go
+++ server/routing/controllers/robot/move_left.go
@@ -7,5 +7,23 @@ import (
 )
 
 func (ctrller controller) MoveLeft(c *gin.Context) {
-	c.JSON(http.StatusOK, map[string]interface{}{})
+	robot, err := ctrller.appCtx.RobotRepository.Get()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"errors": err.Error(),
+		})
+		return
+	}
+
+	err = robot.MoveLeft()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"errors": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"robot": robot,
+	})
 }
